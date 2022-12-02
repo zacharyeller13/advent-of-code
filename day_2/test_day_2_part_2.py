7692fdbda748632447ef74c66caa7084bc2368ca
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2022-12-02 14:58:49 -0600

    Add day 2 part 2

---
+++ day_2/test_day_2_part_2.py
@@ -0,0 +1,52 @@
+CHOICES = {
+    'A': 1, 
+    'B': 2, 
+    'C': 3
+}
+
+WINNERS = {
+    'A': 'C',
+    'B': 'A',
+    'C': 'B'
+}
+
+GAME_OUTCOMES = {
+    'X': 0,
+    'Y': 3,
+    'Z': 6
+}
+
+def read_input(file: str) -> list[str]:
+
+    with open(f"{file}", 'r') as f:
+
+        inputs = f.read()
+        inputs = inputs.split('\n')
+    
+    return inputs
+
+def run_games(games: list[str]) -> list[int]:
+
+    scores = []
+    for game in games:
+        score = GAME_OUTCOMES[game[-1]]
+        if score == 0:
+            score += CHOICES[WINNERS[game[:1]]]
+        elif score == 3:
+            score += CHOICES[game[:1]]
+        else:
+            keys = list(WINNERS.keys())
+            score += CHOICES[keys[list(WINNERS.values()).index(game[:1])]]
+        scores.append(score)
+
+    return scores
+
+def main() -> None:
+    
+    inputs = read_input("test_inputs")
+    scores = run_games(inputs)
+    print(sum(scores))
+
+if __name__ == "__main__":
+
+    main()
