78dd8c50e13763ab1ea408a2703ec47c5d0e9f47
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2022-12-06 18:06:20 -0600

    Try day 6 part 1 in C#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿static int checkMarker(string stream)
+{
+    string group = "";
+    int i = 0;
+
+    while (i <= stream.Length - 4)
+    {
+        group = stream.Substring(i, 4);
+        HashSet<char> groupSet = group.ToHashSet();
+
+        if (group.Length == groupSet.Count)
+        {
+            return i + 4;
+        }
+
+        i++;
+    }
+
+    return -1;
+}
+
+string inputs = System.IO.File.ReadAllText("day_6/inputs");
+
+Console.WriteLine(checkMarker(inputs));
+
