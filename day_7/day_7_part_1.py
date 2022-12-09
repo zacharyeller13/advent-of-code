08b42b2ed1eb3472d9cd3e693728bdd19b1c2906
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2022-12-08 21:52:00 -0600

    Make progress on Day 7 - pass test, fail actual

--- day_7/day_7_part_1.py
+++ day_7/day_7_part_1.py
@@ -3,16 +3,6 @@ from pprint import pprint
 
 from fs_classes import Directory, File
 
-# '/' is root directory
-# '$' precedes commands we execute(d)
-# 'cd' = change directory
-# 'cd x' = looks for directory 'x' and moves into it
-# 'cd ..' = moves out 1 directory
-# 'cd /' = move to root directory
-# 'ls' = list all directories and files in current
-# '123 abc' = filesize 123 filename abc
-# 'dir xyz' = directory named xyz
-
 def read_input(file: str) -> list[str]:
 
     with open(f"{file}", 'r') as f:
@@ -29,21 +19,18 @@ def build_filesystem(filesystem: Directory, inputs: list[str]) -> None:
         input_line = line.split()
 
         if input_line[0] == '$':
-            # print(f"command: {' '.join(input_line[1:])}")
 
             if input_line[1] == 'cd':
+
                 cwd, prev_dir = change_dir(cwd, input_line[-1])
+
                 if cwd == '..':
                     pass
-                    # cwd = filesystem.sub_directories.get(prev_dir).parent_directory
-
-                # print(f"prev_dir: {prev_dir} | cwd: {cwd}")
 
         else:
-            # print(f"file/dir: {input_line}")
             update_filesystem(filesystem, cwd, input_line)
 
-    pprint(filesystem)
+    # pprint(filesystem)
 
 def change_dir(cwd: str, new_cwd: str) -> tuple[str, str]:
 
@@ -58,66 +45,76 @@ def update_filesystem(filesystem: Directory, cwd: str, input_line: list[str]) ->
         new_dir_name = input_line[-1]
 
         if new_dir_name != '/' and cwd == '/' and not filesystem.sub_directories.get(new_dir_name):
-            # use .get(new_dire_name) to check if the directory already exists
+            # use .get(new_dir_name) to check if the directory already exists
             # we don't want to overwrite a dir that already has files in it
 
             filesystem\
             .sub_directories[new_dir_name] = Directory(new_dir_name, parent_directory=cwd)
 
-        elif new_dir_name != '/' and filesystem.sub_directories.get(cwd):
+        else:
+            dir = find_directory(filesystem, cwd)
 
-            filesystem\
-            .sub_directories[cwd]\
-            .sub_directories[new_dir_name] = Directory(new_dir_name, parent_directory=cwd)
+            if dir:
+                dir.sub_directories[new_dir_name] = Directory(new_dir_name, parent_directory=cwd)
 
     else:
         file_size = int(input_line[0])
         file_name = input_line[-1]
 
         if cwd != '/':
-            for dir_name, dir in filesystem.sub_directories.items():
+            dir = find_directory(filesystem, cwd)
 
-                if dir_name == cwd:
-                    filesystem\
-                    .sub_directories[cwd]\
-                    .files[file_name] = File(file_name, file_size, parent_directory=cwd)
-                    
-                else:
-                    for dir_name2, _ in dir.sub_directories.items():
-                        if dir_name2 == cwd:
-                            dir\
-                            .sub_directories[cwd]\
-                            .files[file_name] = File(file_name, file_size, parent_directory=cwd)
+            if dir:
+                dir.files[file_name] = File(file_name, file_size, parent_directory=cwd)
 
         else:
             filesystem.files[file_name] = File(file_name, file_size, parent_directory=cwd)
+
+def find_directory(directory: Directory, dir_to_find: str) -> Directory | None:
+    """
+    Recursively search sub_directories to find a specific `Directory`
+    """
+
+    if dir_to_find in directory.sub_directories:
+        return directory.sub_directories.get(dir_to_find)
+
+    for _, dir in directory.sub_directories.items():
+        if isinstance(dir, Directory):
+            dir = find_directory(dir, dir_to_find)
+            if dir:
+                return dir
+
+    return None
+
+def sum_dir_sizes(directory: Directory, dir_size: int) -> int:
+    
+    dir_sizes = 0
+    for _, dir in directory.sub_directories.items():
+        if dir.total_size() <= dir_size:
+            dir_sizes += dir.total_size()
+        else:
+            dir_sizes += sum_dir_sizes(dir, dir_size)
+
+    return dir_sizes
 
 def create_filesystem() -> Directory:
     """
     Create a new `Directory` object and return it as the root directory
     """
 
-    root_dir = Directory('/')
-
-    return root_dir
+    return Directory('/')
 
 def main() -> None:
 
-    inputs = read_input(f"{os.path.dirname(__file__)}/test_inputs")
-    # commands = get_commands(inputs)
+    inputs = read_input(f"{os.path.dirname(__file__)}/inputs")
 
     filesystem = create_filesystem()
 
-    # print(file_system)
-    # print(commands)
     build_filesystem(filesystem, inputs)
 
-    # root_dir = Directory('/')
-    # root_dir.sub_directories['a'] = Directory('a', parent_directory='/')
-    # root_dir.files['abc'] = File('abc', 100, '/')
-    # pprint(root_dir)
-    # print(root_dir.total_size())
+    small_dirs = sum_dir_sizes(filesystem, 100000)
 
+    print(small_dirs)
 
 if __name__ == "__main__":
     main()
