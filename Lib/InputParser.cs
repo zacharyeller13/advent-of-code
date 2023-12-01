1b7259920fbdec8d6df27210f0b18918db464c90
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2023-12-01 13:56:27 -0600

    Prep for 2023 C# attempts

---
+++ Lib/InputParser.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode.Lib;
+
+public static class InputParser
+{
+    public static List<string> ParseInput(string inputFile)
+    {
+        string[] lines = inputFile.Split('\n');
+        return lines.ToList();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using AdventOfCode.Lib;
+
+var file = InputParser.ParseInput();
+
+Console.WriteLine(file.ToString());
