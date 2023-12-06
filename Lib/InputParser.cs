60505cee3c5ea92aff008fa7f9607c80ee19e2ed
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2023-12-06 12:29:41 -0600

    Update InputParser to handle printing only 1 part or both

--- Lib/InputParser.cs
+++ Lib/InputParser.cs
@@ -2,15 +2,18 @@
 
 public static class InputParser
 {
-    public static string ValidateArgs(string[] args)
+    public static (string, string) ValidateArgs(string[] args) => args.Length switch
     {
-        if (args.Length != 2)
-        {
-            Console.WriteLine("Usage: ./AdventOfCode <year> <day>");
-            Environment.Exit(1);
-        }
-
-        return $"{args[0]}/{args[1]}";
-    }
+        2 => ($"{args[0]}/{args[1]}", "all"),
+        3 => ($"{args[0]}/{args[1]}", args[2]),
+        _ => PrintUsage()
+    };
     public static string[] ParseInput(string inputFile) => File.ReadAllLines(inputFile);
+
+    private static (string, string) PrintUsage()
+    {
+        Console.WriteLine("Usage: ./AdventOfCode <year> <day> <part>");
+        Environment.Exit(1);
+        return ("", "");
+    }
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using AdventOfCode._2023.day_04;
 using AdventOfCode.Lib;
 
 
-string inputFolder = InputParser.ValidateArgs(args);
+(string inputFolder, string part) = InputParser.ValidateArgs(args);
 
 string[] testFile = InputParser.ParseInput($"./{inputFolder}/part1TestInput.txt");
 string[] file = InputParser.ParseInput($"./{inputFolder}/input.txt");
@@ -13,11 +13,18 @@ string divider = new string('-', 25);
 Scratchcards testSolution = new(testFile);
 Scratchcards solution = new(file);
 
-Console.WriteLine($"{divider}Part 1{divider}");
-Console.WriteLine(testSolution.SolvePart1());
-Console.WriteLine(solution.SolvePart1());
+if (part == "1" || part == "all")
+{
+    Console.WriteLine($"{divider}Part 1{divider}");
+    Console.WriteLine(testSolution.SolvePart1());
+    Console.WriteLine(solution.SolvePart1());
+}
 
 // Part 2 Solution
-Console.WriteLine($"{divider}Part 2{divider}");
-Console.WriteLine(testSolution.SolvePart2());
-Console.WriteLine(solution.SolvePart2());
+
+if (part == "2" || part == "all")
+{
+    Console.WriteLine($"{divider}Part 2{divider}");
+    Console.WriteLine(testSolution.SolvePart2());
+    Console.WriteLine(solution.SolvePart2());
+}
