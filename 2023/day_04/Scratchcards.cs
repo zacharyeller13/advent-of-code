9db8698a7d25e841bc0a40ea8c23d15a02b91645
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2023-12-05 23:29:37 -0600

    Update day 4 part 1 with possible faster solution
    Adds allocations for the sets, but finding the intersection is much faster

--- 2023/day_04/Scratchcards.cs
+++ 2023/day_04/Scratchcards.cs
@@ -5,25 +5,30 @@ namespace AdventOfCode._2023.day_04;
 public class Scratchcards : ISolution
 {
     private readonly string[] _lines;
+    private readonly List<int[]> _scores = new();
+    private readonly List<int[]> _scratchers = new();
 
     public Scratchcards(string[] fileContents)
     {
         _lines = fileContents;
+        foreach (var line in _lines)
+        {
+            _scores.Add(GetScores(line));
+            _scratchers.Add(GetScratchers(line));
+        }
     }
 
     public int SolvePart1()
     {
-        int[] scores;
-        int[] scratches;
         int totalScore = 0;
 
-        foreach (var line in _lines)
+        for (int i = 0; i < _scores.Count; i++)
         {
             int scoreCount = -1;
-            scores = GetScores(line);
-            scratches = GetScratches(line);
 
-            scoreCount += scratches.Where(n => scores.Contains(n)).Count();
+            var scratchSet = new HashSet<int>(_scratchers[i]);
+            var scoreSet = new HashSet<int>(_scores[i]);
+            scoreCount += scoreSet.Intersect(scratchSet).Count();
 
             totalScore += 1 << scoreCount;
         }
@@ -33,6 +38,12 @@ public class Scratchcards : ISolution
 
     public int SolvePart2()
     {
+        // For each card, check winners
+        // For each winner, add an additional next card
+        // (e.g. Card 1 wins 2, add additional Card 2 and additional Card 3)
+        // Repeat until no more cards to process
+        // How many total cards are processed after this?
+        // Constraint: Cards will never make you copy a card past the end of the table
         throw new NotImplementedException();
     }
     public void PrintLines()
@@ -54,9 +65,11 @@ public class Scratchcards : ISolution
         return Array.ConvertAll<string, int>(scores, (n => int.Parse(n)));
     }
 
-    private static int[] GetScratches(string card)
+    private static int[] GetScratchers(string card)
     {
-        string[] scratches = card.Split('|')[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string[] scratches = card.Split('|')[1]
+                                .Trim()
+                                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
         return Array.ConvertAll<string, int>(scratches, (n => int.Parse(n.Trim())));
     }
