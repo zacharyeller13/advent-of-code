4cd020bc7ca94aff4645d8f65261b1f0147d08db
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2023-12-05 23:03:53 -0600

    Add Day 4 Part 1 Solution

---
+++ 2023/day_04/Scratchcards.cs
@@ -0,0 +1,63 @@
+﻿using AdventOfCode.Lib;
+
+namespace AdventOfCode._2023.day_04;
+
+public class Scratchcards : ISolution
+{
+    private readonly string[] _lines;
+
+    public Scratchcards(string[] fileContents)
+    {
+        _lines = fileContents;
+    }
+
+    public int SolvePart1()
+    {
+        int[] scores;
+        int[] scratches;
+        int totalScore = 0;
+
+        foreach (var line in _lines)
+        {
+            int scoreCount = -1;
+            scores = GetScores(line);
+            scratches = GetScratches(line);
+
+            scoreCount += scratches.Where(n => scores.Contains(n)).Count();
+
+            totalScore += 1 << scoreCount;
+        }
+
+        return totalScore;
+    }
+
+    public int SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+    public void PrintLines()
+    {
+        foreach (var line in _lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    private static int[] GetScores(string card)
+    {
+        string[] scores = card.Split(':')[1]
+                            .Trim()
+                            .Split('|')[0]
+                            .Trim()
+                            .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        return Array.ConvertAll<string, int>(scores, (n => int.Parse(n)));
+    }
+
+    private static int[] GetScratches(string card)
+    {
+        string[] scratches = card.Split('|')[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        return Array.ConvertAll<string, int>(scratches, (n => int.Parse(n.Trim())));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode._2023.day_03;
+using AdventOfCode._2023.day_04;
 using AdventOfCode.Lib;
 
 
@@ -9,14 +10,14 @@ string[] file = InputParser.ParseInput($"./{inputFolder}/input.txt");
 string divider = new string('-', 25);
 
 // Part 1 Solution
-GearRatios testSolution = new(testFile);
-GearRatios solution = new(file);
+Scratchcards testSolution = new(testFile);
+Scratchcards solution = new(file);
 
 Console.WriteLine($"{divider}Part 1{divider}");
-Console.WriteLine(testSolution.SolvePart1()); // Expect: 4361
-Console.WriteLine(solution.SolvePart1()); // 489676 too low; 531561 correct
+Console.WriteLine(testSolution.SolvePart1());
+Console.WriteLine(solution.SolvePart1());
 
 // Part 2 Solution
 Console.WriteLine($"{divider}Part 2{divider}");
-Console.WriteLine(testSolution.SolvePart2()); // Expect: 467835
-Console.WriteLine(solution.SolvePart2()); // 83279367 correct
+//Console.WriteLine(testSolution.SolvePart2());
+//Console.WriteLine(solution.SolvePart2());
