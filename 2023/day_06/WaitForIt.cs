c3fc5ab3be2948dc554ecf2b2fd9d1d6eff8281e
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2023-12-07 21:47:42 -0600

    Add Day 6 Part 1 and 2 solutions

---
+++ 2023/day_06/WaitForIt.cs
@@ -0,0 +1,70 @@
+﻿using AdventOfCode.Lib;
+
+namespace AdventOfCode._2023.day_06;
+
+
+// Start speed = 0
+// Speed added per ms held: 1
+// So 1 ms = 1 mm/ms; 2 ms = 2 mm/ms; 3 ms = 3 mm/ms
+public class WaitForIt : SolutionBase
+{
+    private long[] _times;
+    private long[] _distances;
+    public WaitForIt(string[] fileContents) : base(fileContents)
+    {
+        _times = ParseLine(0, splitlongs: true);
+        _distances = ParseLine(1, splitlongs: true);
+    }
+
+    public long SolvePart1()
+    {
+        return GetPossibleWins().Aggregate((x, y) => x * y);
+    }
+
+    public long SolvePart2()
+    {
+        // Reset because the digits should be all together
+        _times = ParseLine(0);
+        _distances = ParseLine(1);
+        
+        return GetPossibleWins().Aggregate((x, y) => x * y);
+    }
+
+    private long[] ParseLine(long index, bool splitlongs = false)
+    {
+        string[] digits;
+        string line = _lines[index].Split(':')[1];
+        if (splitlongs)
+        {
+            digits = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+        else
+        {
+            digits = new string[] { line.Trim().Replace(" ", "") };
+        }
+        return Array.ConvertAll<string, long>(digits, long.Parse);
+    }
+    
+    private IEnumerable<long> GetPossibleWins()
+    {
+        for (long i = 0; i < _times.Length; i++)
+        {
+            long wins = 0;
+            long duration = _times[i];
+            long record = _distances[i];
+
+            for (long j = 1; j < duration; j++)
+            {
+                // speed * timeTraveled
+                long distanceTraveled = j * (duration - j);
+
+                if (distanceTraveled > record)
+                {
+                    wins++;
+                }
+                
+            }
+            yield return wins;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
-﻿using AdventOfCode._2023.day_05;
+﻿using AdventOfCode._2023.day_06;
 using AdventOfCode.Lib;
 
 (string inputFolder, string part) = InputParser.ValidateArgs(args);
 
-string[] testFile = InputParser.ParseInput($"./{inputFolder}/part1TestInput.txt", $"{Environment.NewLine}{Environment.NewLine}");
-string[] file = InputParser.ParseInput($"./{inputFolder}/input.txt", $"{Environment.NewLine}{Environment.NewLine}");
+string[] testFile = InputParser.ParseInput($"./{inputFolder}/part1TestInput.txt");
+string[] file = InputParser.ParseInput($"./{inputFolder}/input.txt");
 string divider = new string('-', 25);
 
 // Part 1 Solution
-Seeds testSolution = new(testFile);
-Seeds solution = new(file);
+WaitForIt testSolution = new(testFile);
+WaitForIt solution = new(file);
 
 if (part == "1" || part == "all")
 {
     Console.WriteLine($"{divider}Part 1{divider}");
-    Console.WriteLine(testSolution.SolvePart1Long()); // Expect 35 as final output
-    Console.WriteLine(solution.SolvePart1Long()); // 178159714 correct
+    Console.WriteLine(testSolution.SolvePart1());
+    Console.WriteLine(solution.SolvePart1());
 }
 
 // Part 2 Solution
@@ -23,6 +23,6 @@ if (part == "1" || part == "all")
 if (part == "2" || part == "all")
 {
     Console.WriteLine($"{divider}Part 2{divider}");
-    Console.WriteLine(testSolution.SolvePart2Long());
-    //Console.WriteLine(solution.SolvePartLong2());
+    Console.WriteLine(testSolution.SolvePart2());
+    Console.WriteLine(solution.SolvePart2());
 }
