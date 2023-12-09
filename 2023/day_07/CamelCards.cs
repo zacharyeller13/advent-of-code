a9c17d3b6591f75d6fb726b435f83a4b33d79bfc
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2023-12-09 13:32:14 -0600

    Remove unused CardWeights dict
    Doesn't work with that dict, because the values of facecards were 2 digits; which when comparing chars, ends up always comparing as each individual digit, so ends up lower than wanted.

--- 2023/day_07/CamelCards.cs
+++ 2023/day_07/CamelCards.cs
@@ -6,24 +6,7 @@ namespace AdventOfCode._2023.day_07;
 
 public class CamelCards : SolutionBase
 {
-    private static readonly Dictionary<char, int> CardWeights = new()
-    {
-        { '2', 2 },
-        { '3', 3 },
-        { '4', 4 },
-        { '5', 5 },
-        { '6', 6 },
-        { '7', 7 },
-        { '8', 8 },
-        { '9', 9 },
-        { 'T', 10 },
-        { 'J', 11 },
-        { 'Q', 12 },
-        { 'K', 13 },
-        { 'A', 14 },
-    };
-
-    private Dictionary<string, int> _handBids = new();
+    private readonly Dictionary<string, int> _handBids = new();
 
     public CamelCards(string[] fileContents) : base(fileContents)
     {
