0211c3761a47dd1520f4f7df9919c5c93b9a8f61
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2023-12-12 22:49:15 -0600

    Add day 9 part 1 solution

---
+++ 2023/day_09/MirageMaintenance.cs
@@ -0,0 +1,54 @@
+using AdventOfCode.Lib;
+
+namespace AdventOfCode._2023.day_09;
+
+public class MirageMaintenance : SolutionBase
+{
+    public MirageMaintenance(string[] fileContents) : base(fileContents)
+    {
+    }
+
+    public override int SolvePart1()
+    {
+        int extrapolatedValues = 0;
+        foreach (string line in _lines)
+        {
+            int[] intLine = Array.ConvertAll(line.Split(), int.Parse);
+            // Create sequences stack with each subsequent sequence till we reach 
+            // a sequence of all zeros
+            Stack<int[]> sequences = FindZeroSequence(intLine);
+
+            // Pop each sequence and add a new value at the end
+            // without actually adding it to the array
+            // just calculate what the new value would be one each sequence
+            // until we get to the top line
+            int nextInt = 0;
+            while (sequences.Count > 0)
+            {
+                int[] sequence = sequences.Pop();
+                nextInt += sequence[^1];
+            }
+            extrapolatedValues += (nextInt + intLine[^1]);
+        }
+
+        return extrapolatedValues;
+    }
+
+    private static Stack<int[]> FindZeroSequence(int[] intLine)
+    {
+        // Add the initial line to the stack for processing on reversal?
+        Stack<int[]> sequences = new();
+            
+        while (intLine.Any(n => n != 0))
+        {
+            int[] sequence = new int[intLine.Length - 1];
+            for (int i = 0; i < intLine.Length - 1; i++)
+            {
+                sequence[i] = intLine[i + 1] - intLine[i];
+            }
+            sequences.Push(sequence);
+            intLine = sequence;
+        }
+        return sequences;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,34 +1,28 @@
-﻿using AdventOfCode._2023.day_08;
+﻿using AdventOfCode._2023.day_09;
 using AdventOfCode.Lib;
 
 (string inputFolder, string part) = InputParser.ValidateArgs(args);
 
 string[] testFile = InputParser.ParseInput($"./{inputFolder}/part1TestInput.txt", "\n");
-string[] testFile2 = InputParser.ParseInput($"./{inputFolder}/part2TestInput.txt", "\n");
 string[] file = InputParser.ParseInput($"./{inputFolder}/input.txt", "\n");
 string divider = new string('-', 25);
 
 // Part 1 Solution
-HauntedWasteland testSolution = new(testFile);
-HauntedWasteland solution = new(file);
+MirageMaintenance testSolution = new(testFile);
+MirageMaintenance solution = new(file);
 
 if (part is "1" or "all")
 {
     Console.WriteLine($"{divider}Part 1{divider}");
-    Console.WriteLine(testSolution.SolvePart1());
-    Console.WriteLine(solution.SolvePart1());
+    Console.WriteLine(testSolution.SolvePart1()); // 18, 28, 68 = 114
+    Console.WriteLine(solution.SolvePart1()); // 1877825184
 }
 
 // Part 2 Solution
 
 if (part is "2" or "all")
 {
-    HauntedWasteland testSolution2 = new(testFile2);
     
     Console.WriteLine($"{divider}Part 2{divider}");
-    Console.WriteLine(testSolution2.SolvePart2());
-    Console.WriteLine(solution.SolvePart2()); // 1016719625 too low; 14321394058031 correct
-    
-    Console.WriteLine(MathExtensions.GCD2(13019, 16897, 16343, 21883, 14681, 20221));
-    Console.WriteLine(MathExtensions.LCM2(13019, 16897, 16343, 21883, 14681, 20221));
+    Console.WriteLine(solution.SolvePart2());
 }
