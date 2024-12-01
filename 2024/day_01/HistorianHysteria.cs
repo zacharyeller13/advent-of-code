5b3481e6febcd8daf45cc6a0816900ed959b08d8
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2024-12-01 11:52:12 -0600

    feat(2024): Day 1 Part 1 solved

---
+++ 2024/day_01/HistorianHysteria.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode.Lib;
+
+namespace AdventOfCode._2024.day_01;
+
+/// <summary>
+/// In the example, the pairs and distances would be as follows:
+///     l r, distance
+///     1 3, 2
+///     2 3, 1
+///     3 3, 0
+///     3 4, 1
+///     3 5, 2
+///     4 9, 5
+///
+/// To find the total distance between the left list and the right list, add up the distances between all of the pairs you found.
+/// In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
+/// </summary>
+public class HistorianHysteria : SolutionBase<int>
+{
+    private readonly List<int> _left = [];
+    private readonly List<int> _right = [];
+    
+    public HistorianHysteria(string[] fileContents) : base(fileContents)
+    {
+        foreach (string line in _lines)
+        {
+            string[] nums = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            _left.Add(int.Parse(nums[0]));
+            _right.Add(int.Parse(nums[1]));
+        }
+        _left.Sort();
+        _right.Sort();
+    }
+
+    public override int SolvePart1()
+    {
+        int sum = 0;
+        for (int i = 0; i < _left.Count; i++)
+        {
+            sum += Math.Abs(_left[i] - _right[i]);
+        }
+        return sum;
+    }
+
+    public override int SolvePart2()
+    {
+        return -1;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,20 +19,20 @@ Type solutionType = types[0];
 
 if (solutionType.BaseType?.GenericTypeArguments[0] == typeof(int))
 {
-    var testSolution = Activator.CreateInstance(solutionType, args: [testFile]);
-    var solution = Activator.CreateInstance(solutionType, args: [file]);
+    object? testSolution = Activator.CreateInstance(solutionType, args: [testFile]);
+    object? solution = Activator.CreateInstance(solutionType, args: [file]);
     RunSolutions((testSolution as SolutionBase<int>)!, (solution as SolutionBase<int>)!);
 }
 else if (solutionType.BaseType?.GenericTypeArguments[0] == typeof(long))
 {
-    var testSolution = Activator.CreateInstance(solutionType, args: [testFile]);
-    var solution = Activator.CreateInstance(solutionType, args: [file]);
+    object? testSolution = Activator.CreateInstance(solutionType, args: [testFile]);
+    object? solution = Activator.CreateInstance(solutionType, args: [file]);
     RunSolutions((testSolution as SolutionBase<long>)!, (solution as SolutionBase<long>)!);
 }
 else if (solutionType.BaseType?.GenericTypeArguments[0] == typeof(ulong))
 {
-    var testSolution = Activator.CreateInstance(solutionType, args: [testFile]);
-    var solution = Activator.CreateInstance(solutionType, args: [file]);
+    object? testSolution = Activator.CreateInstance(solutionType, args: [testFile]);
+    object? solution = Activator.CreateInstance(solutionType, args: [file]);
     RunSolutions((testSolution as SolutionBase<ulong>)!, (solution as SolutionBase<ulong>)!);
 }
 
@@ -44,15 +44,15 @@ void RunSolutions<T>(SolutionBase<T> testSolution, SolutionBase<T> solution)
     if (part is "1" or "all")
     {
         Console.WriteLine($"{divider}Part 1{divider}");
-        Console.WriteLine(testSolution.SolvePart1()); // 18, 28, 68 = 114
-        Console.WriteLine(solution.SolvePart1()); // 1877825184
+        Console.WriteLine(testSolution.SolvePart1());
+        Console.WriteLine(solution.SolvePart1());
     }
 
     // Part 2 Solution
     if (part is "2" or "all")
     {
         Console.WriteLine($"{divider}Part 2{divider}");
-        Console.WriteLine(testSolution.SolvePart2()); // 2
-        Console.WriteLine(solution.SolvePart2()); // 1108
+        Console.WriteLine(testSolution.SolvePart2());
+        Console.WriteLine(solution.SolvePart2());
     }
 }
